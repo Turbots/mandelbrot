a9f7e092fa673e0a562bd944e6f146a4b7a5fb43
Dieter Hubau <dhubau@gmail.com>
2018-09-25 20:12:52 +0200

    Updated some code and fixed calculation

---
+++ src/main/java/be/ordina/function/Input.java
@@ -0,0 +1,13 @@
+package be.ordina.function;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Input {
+
+	private double real;
+	private double imaginary;
+	private int max;
+}
--- src/main/java/be/ordina/function/MandelbrotFunction.java
+++ src/main/java/be/ordina/function/MandelbrotFunction.java
@@ -3,21 +3,24 @@ package be.ordina.function;
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
-public class MandelbrotFunction implements Function<String, Integer> {
+public class MandelbrotFunction implements Function<Input, Integer> {
 
-	public Integer apply(String input) {
+	public Integer apply(Input input) {
 		Complex complex;
 		Integer max;
 
-		try {
+		/**try {
 			complex = new Complex(
 				Double.parseDouble(input.substring(0, input.indexOf(','))),
-				Double.parseDouble(input.substring(input.indexOf(','), input.lastIndexOf(','))));
+				Double.parseDouble(input.substring(input.indexOf(',') + 1, input.lastIndexOf(','))));
 
-			max = Integer.parseInt(input.substring(input.lastIndexOf(',')));
+			max = Integer.parseInt(input.substring(input.lastIndexOf(',') + 1));
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Wrong format: use `<real>,<imaginary>,<max>`");
-		}
+		}**/
+
+		complex = new Complex(input.getReal(), input.getImaginary());
+		max = input.getMax();
 
 		Complex z = new Complex(complex);
 
