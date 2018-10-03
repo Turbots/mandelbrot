07592aebeb2f4bfbb9a09c6294618ea7997da810
Dieter Hubau <dhubau@gmail.com>
2018-10-03 21:23:03 +0200

    Added Flux to the equation

--- src/main/java/be/ordina/function/MandelbrotFunction.java
+++ src/main/java/be/ordina/function/MandelbrotFunction.java
@@ -1,22 +1,26 @@
 package be.ordina.function;
 
+import reactor.core.publisher.Flux;
+
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
-public class MandelbrotFunction implements Function<Calculation, Integer> {
+public class MandelbrotFunction implements Function<Flux<Calculation>, Flux<Integer>> {
 
-	public Integer apply(Calculation calculation) {
-		Complex complex = new Complex(calculation.getReal(), calculation.getImaginary());
-		Integer max = calculation.getMax();
+	public Flux<Integer> apply(Flux<Calculation> calculation) {
+		return calculation.map(c -> {
+			Complex complex = new Complex(c.getReal(), c.getImaginary());
+			Integer max = c.getMax();
 
-		Complex z = new Complex(complex);
+			Complex z = new Complex(complex);
 
-		for (int t = 0; t < max; t++) {
-			if (z.abs() > 2.0)
-				return t;
-			z = z.times(z).plus(complex);
-		}
+			for (int t = 0; t < max; t++) {
+				if (z.abs() > 2.0)
+					return t;
+				z = z.times(z).plus(complex);
+			}
 
-		return max;
+			return max;
+		});
 	}
 }
